46bce6dad702329ea4bd265a85c087f23cbec10c
JesseNReynolds <jessenreynolds@gmail.com>
2021-01-18 10:25:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(arrOfObj) {
+  const win = arrOfObj.find(function(obj){
+    return obj.result === "W"
+  })
+  if (win) {
+    return win.year
+  }
 }
